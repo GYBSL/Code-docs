3ab1995e5ba633006b3f004e63a351802e039b2d
GYBSL <1290005228@qq.com>
2022-09-17 17:36:57 +0800

    更换了logo

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'Code-docs',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  title: '学习笔记文档',
+  favicon: 'https://gitee.com/gybsl/image-upload/raw/master/image_docs/gybsl.png',
+  logo: 'https://gitee.com/gybsl/image-upload/raw/master/image_docs/gybsl.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  // more config: https://d.umijs.org/config
 });
